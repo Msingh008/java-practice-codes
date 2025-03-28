07cb22ec6f8dd5e2e32d7cb44bd0d721799ea7c8
Manish Singh <msingh30497@gmail.com>
2025-03-28 12:56:31 +0530

    Deleted files

--- src/practiceday/Commit.java
+++ src/practiceday/Commit.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.err.println("pata nhi kya ho rah hai");
 	System.out.println("Phir se commit start kartah u.....");
 	System.out.println("Wanto make a push Knowling my remote is one commit ahead of me...");
+	System.out.println("One more line added");
 }
 }
