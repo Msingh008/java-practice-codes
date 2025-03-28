6520b964becadf92868abf89ce6416db4408beb2
Manish Singh <msingh30497@gmail.com>
2025-03-28 12:15:09 +0530

    Commiting again after merging git hun main...

---
+++ src/practiceday/Commit.java
@@ -0,0 +1,8 @@
+package practiceday;
+
+public class Commit {
+public static void main(String[] args) {
+	System.err.println("pata nhi kya ho rah hai");
+	System.out.println("Phir se commit start kartah u.....");
+}
+}
