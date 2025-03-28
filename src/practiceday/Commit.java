4c9db36bdad24e4f660fd4691dbe16f294090563
Manish Singh <msingh30497@gmail.com>
2025-03-28 12:35:05 +0530

    I am trying to merge to push

--- src/practiceday/Commit.java
+++ src/practiceday/Commit.java
@@ -4,5 +4,6 @@ public class Commit {
 public static void main(String[] args) {
 	System.err.println("pata nhi kya ho rah hai");
 	System.out.println("Phir se commit start kartah u.....");
+	System.out.println("Wanto make a push Knowling my remote is one commit ahead of me...");
 }
 }
